0c207cdbb127b001cfd283005b6540b6229a69b9
omrixo11 <mohamedali.omri@esprit.tn>
2022-11-11 20:01:30 +0100

    6th Commit

--- app/src/main/java/com/esprit/lunar/PlusFragment.java
+++ app/src/main/java/com/esprit/lunar/PlusFragment.java
@@ -1,66 +1,59 @@
 package com.esprit.lunar;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlusFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PlusFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PlusFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlusFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlusFragment newInstance(String param1, String param2) {
-        PlusFragment fragment = new PlusFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class PlusFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_plus, container, false);
+
+        Spinner spinnerBrand = v.findViewById(R.id.spinnerCarBrand);
+        Spinner spinnerModel = v.findViewById(R.id.spinnerCarModel);
+        Spinner spinnerYear = v.findViewById(R.id.spinnerCarYear);
 
 
-        return inflater.inflate(R.layout.fragment_plus, container, false);
+        ArrayAdapter<CharSequence> adapterBrand = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carBrands, android.R.layout.simple_spinner_item);
+        adapterBrand.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterModel = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carModel, android.R.layout.simple_spinner_item);
+        adapterModel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterYear = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carYear, android.R.layout.simple_spinner_item);
+        adapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinnerBrand.setAdapter(adapterBrand);
+        spinnerBrand.setOnItemSelectedListener(this);
+
+        spinnerModel.setAdapter(adapterModel);
+        spinnerModel.setOnItemSelectedListener(this);
+
+        spinnerYear.setAdapter(adapterYear);
+        spinnerYear.setOnItemSelectedListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/esprit/lunar/SearchFragment.java
+++ app/src/main/java/com/esprit/lunar/SearchFragment.java
@@ -7,58 +7,54 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class SearchFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+
+        Spinner spinnerBrand = v.findViewById(R.id.spinnerCarBrand);
+        Spinner spinnerModel = v.findViewById(R.id.spinnerCarModel);
+        Spinner spinnerYear = v.findViewById(R.id.spinnerCarYear);
+
+
+        ArrayAdapter<CharSequence> adapterBrand = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carBrands, android.R.layout.simple_spinner_item);
+        adapterBrand.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterModel = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carModel, android.R.layout.simple_spinner_item);
+        adapterModel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterYear = ArrayAdapter.createFromResource(getActivity().getBaseContext(),R.array.carYear, android.R.layout.simple_spinner_item);
+        adapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinnerBrand.setAdapter(adapterBrand);
+        spinnerBrand.setOnItemSelectedListener(this);
+
+        spinnerModel.setAdapter(adapterModel);
+        spinnerModel.setOnItemSelectedListener(this);
+
+        spinnerYear.setAdapter(adapterYear);
+        spinnerYear.setOnItemSelectedListener(this);
+
+        return  v;
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
